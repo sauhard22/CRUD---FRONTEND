aac5d5e72caf4c5ac7fb28747e3f91a8186f343c
sauhard22 <73341891+sauhard22@users.noreply.github.com>
2021-07-17 01:02:16 +0530

    pushed to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css"
+import {useState, useEffect} from 'react'
+import Axios from 'axios'
 
 function App() {
+
+  const [movieName, setMovieName] = useState('')
+  const [review, setReview] = useState('')
+  const [movieReviewList, setMovieList] = useState([])
+
+  useEffect(()=>{
+    Axios.get("http://localhost:3001/api/get").then((response)=>{
+      setMovieList(response.data)
+    })
+  },[])
+
+  const submitReview = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      movieName: movieName, movieReview: review
+    })
+
+    setMovieList([
+      ...movieReviewList, {
+        movieName: movieName,
+         movieReview: review
+        }])
+  }
+
+  const deleteReview = (movie) => {
+    Axios.delete(`http://localhost:3001/api/delete/${movie}`)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>CRUD APPLICATION</h1>
+      <div className="form">
+        <label>Movie Name</label>
+          <input type="text" name="movieName" onChange={(e)=>{
+            setMovieName(e.target.value)
+          }}/>
+        <label>Movie Review</label>
+          <input type="text" name="review" onChange={(e)=>{
+            setReview(e.target.value)
+          }}/>
+          <button onClick={submitReview}>Submit</button>
+
+          {movieReviewList.map ((val)=>{
+            return(
+            <div className="card ">
+            <h1>{val.movieName}</h1>
+            <p>{val.movieReview}</p>
+
+            <button onClick={() => {deleteReview(val.movieName)}}>Delete</button>
+            <input type="text" id="updateInput"/>
+            <button>Update</button>
+            </div>
+          )
+          })}
+      </div>
     </div>
   );
 }
